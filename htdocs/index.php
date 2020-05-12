e2d42fd893fcef9c6bd44e651b6e554ad0587f8c
spagbol <38818825+pozorfluo@users.noreply.github.com>
2020-05-12 10:34:19 +0200

    Scrape some placeholder data

--- htdocs/index.php
+++ htdocs/index.php
@@ -30,8 +30,40 @@ if (session_status() === PHP_SESSION_NONE) {
      
 
 //--------------------------------------------------------------- playground
+$ph_data_path = ROOT . 'resources/data/ph_data_products.json';
+$ph_data = json_decode(file_get_contents($ph_data_path), true);
+
+// echo '<pre>'.var_export($ph_data, true).'</pre><hr />';
+
+foreach($ph_data['data']['posts']['edges'] as $product) {
+    // echo '<pre>'.var_export($product['node'], true).'</pre><hr />';
+
+    // (`created_at`, `name`, `summary`, `website`, `thumbnail`, 0)
+    echo '(' 
+        .(new DateTime($product['node']['createdAt']))->format('Y-m-d H:i:s').', '
+        .$product['node']['name'].', '
+        .$product['node']['tagline'].', '
+        .$product['node']['website'].', '
+        .$product['node']['thumbnail']['url'].', '
+        .'0),<br />';
+
+        $thumbnail = file_get_contents($product['node']['thumbnail']['url']);
+        $thumbnail_path = 'public/images/products/thumbnails/'.str_replace(' ', '-', $product['node']['name']). uniqid() .'.webp';
+        echo '<pre>'.var_export($thumbnail_path, true).'</pre><hr />';
+        file_put_contents($thumbnail_path, $thumbnail);
+
+        foreach($product['node']['media'] as $index => $media) {
+            $image = file_get_contents($media['url']);
+            $image_path = 'public/images/products/'.str_replace(' ', '-', $product['node']['name']).'_'. $index .'_'. uniqid() .'.webp';
+            echo '<pre>'.var_export($image_path, true).'</pre><hr />';
+            file_put_contents($image_path, $image);
+        }
+}
 
 
+
+$php_datetime = new DateTime('2020-05-10T07:01:00Z');
+echo '<pre>'.var_export($php_datetime->format('Y-m-d H:i:s'), true).'</pre><hr />';
 //---------------------------------------------------------------------- run
 
 //------------------------------------------------------------------- config
---
+++ htdocs/resources/sql/tp_product_hunt_ph_data.sql
@@ -0,0 +1,31 @@
+-- Host: mysql
+-- PHP Version: 7.3.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+--
+-- Database: `tp_product_hunt`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Placeholder data for table `products`
+--
+
+INSERT INTO `products` (`created_at`, `name`, `summary`, `website`, `thumbnail`, `votes_count`) VALUES
+(`created_at`, `name`, `summary`, `website`, `thumbnail`, 0)
+
+'2020-05-07 17:40:34'
+'2020-05-07 17:41:46'
+'2020-05-07 17:42:00'
+'2020-05-07 17:43:23'
+'2020-05-07 17:44:07'
+'2020-05-07 17:44:23'
+'2020-05-07 17:44:34'
+'2020-05-07 17:44:53'
+'2020-05-07 17:44:56'
+'2020-05-07 17:45:19'
